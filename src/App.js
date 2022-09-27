2332a775caa52cc1886c374b0f062eea6054f508
ravikrushna963 <ravikrushna963@gmail.com>
2022-09-27 18:02:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    color1: "white1",
+    color2: "white2",
+    color3: "white3",
+  };
+
+  stopClick = () => {
+    this.setState({ color1: "redlight", color2: "white2", color3: "white3" });
+  };
+
+  readyClick = () => {
+    this.setState({
+      color1: "white1",
+      color2: "yellowlight",
+      color3: "white3",
+    });
+  };
+
+  goClick = () => {
+    this.setState({ color1: "white1", color2: "white2", color3: "greenlight" });
+  };
+
+  render() {
+    const { color1, color2, color3 } = this.state;
+    return (
+      <div className="bg-container">
+        <h1 className="heading1">Traffic Light</h1>
+        <div>
+          <div className="d-flex flex-row justify-content-center">
+            <div>
+              <div>
+                <button
+                  type="button"
+                  className="button bg-danger"
+                  onClick={this.stopClick}
+                >
+                  Stop
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="button bg-warning"
+                  onClick={this.readyClick}
+                >
+                  Ready
+                </button>
+              </div>
+              <div>
+                <button type="button" className="button bg-success" onClick={this.goClick}>
+                  Go
+                </button>
+              </div>
+            </div>
+            <div>
+              <div className="card-container">
+                <p id="stopLight" className={color1}></p>
+                <p id="readyLight" className={color2}></p>
+                <p id="goLight" className={color3}></p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
